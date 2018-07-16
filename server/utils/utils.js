969c26dcce6f162189fc565e30fafa02c844f3e9
AlucardChang <lichang_0716@sina.com>
2018-07-16 15:02:32 +0800

    backend fixed

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -14,14 +14,14 @@ function ADD_STORE(req, res, productID, quantity) {
 
     /*商品重复性检测*/
     //当session中的未支付商品和json文件中未支付商品重复时，累加数量
-    let itemObj = {productID, quantity},
+    let itemObj = {productID, quantity,personID},
         dupMark = false;
     if (req.storeDATA.length > 0) {
         let arrItem = null;
         for (let i = 0; i < req.storeDATA.length; i++) {
             arrItem = req.storeDATA[i];
             console.log(itemObj);
-            if (itemObj.productID == arrItem.productID && (arrItem && arrItem.state == 0)) {
+            if (itemObj.productID == arrItem.productID && (arrItem && arrItem.state == 0)&&itemObj.personID == arrItem.personID) {
                 dupMark = true;
                 break;
             }
